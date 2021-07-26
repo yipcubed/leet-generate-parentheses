b2d7fd2be8bb799618d9ba9cb2d1dc8babc3fc3e
Yusuf Pisan <pisan@uw.edu>
2021-07-26 15:50:20 -0700

    start

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <stack>
 #include <vector>
 
+#include "../utils/PrintUtils.hpp"
+
 using namespace std;
 
 // Live coding problems, watch at
@@ -25,12 +27,54 @@ static auto x = []() {
 
 class Solution {
 public:
-  // alternative is to sort and test numbers next to each other
-  // set is easy, but wastes some space
-  bool isPowerOfTwo(int n) {}
+  vector<string> generateParenthesis(int n) {
+    // PV1(n);
+    if (!n) {
+      return {""};
+    }
+    vector<string> parens;
+    for (int i = 0; i < n; i++) {
+      for (string l : generateParenthesis(i)) {
+        for (string r : generateParenthesis(n - 1 - i)) {
+          // PV2(i, n - 1 - i);
+          // PV1(l);
+          // PV1(r);
+          parens.push_back('(' + l + ')' + r);
+        }
+      }
+    }
+    // PV2(n, parens);
+    return parens;
+  }
+
+  vector<string> generateParenthesis2(int n) {
+    vector<string> res;
+    addingpar(res, "", n, 0);
+    return res;
+  }
+  void addingpar(vector<string> &v, string str, int n, int m) {
+    if (n == 0 && m == 0) {
+      v.push_back(str);
+      return;
+    }
+    if (m > 0) {
+      addingpar(v, str + ")", n, m - 1);
+    }
+    if (n > 0) {
+      addingpar(v, str + "(", n - 1, m + 1);
+    }
+  }
 };
 
-void test1() {}
+void test1() {
+  Solution S;
+  cout << "----" << endl;
+  S.generateParenthesis(1);
+  cout << "----" << endl;
+  S.generateParenthesis(2);
+  cout << "----" << endl;
+  S.generateParenthesis(3);
+}
 
 void test2() {}
 
